eceff635efb5da66ca347a47459022a55aacd6b4
mf81 <maciej.fijalkowski81@gmail.com>
2018-04-07 13:09:38 +0200

    first add

---
+++ src/main/java/eu/maciejfijalkowski/superchat/controlers/MainController.java
@@ -0,0 +1,14 @@
+package eu.maciejfijalkowski.superchat.controlers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+}
---
+++ src/main/java/eu/maciejfijalkowski/superchat/models/ChatSocket.java
@@ -0,0 +1,44 @@
+package eu.maciejfijalkowski.superchat.models;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@EnableWebSocket
+@Component
+public class ChatSocket extends TextWebSocketHandler implements WebSocketConfigurer {
+
+    List<WebSocketSession> userList = new ArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        userList.add(session);
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
+        webSocketHandlerRegistry
+                .addHandler(this,"/chat")
+                .setAllowedOrigins("*");
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        for (WebSocketSession webSocketSession: userList){
+            webSocketSession.sendMessage(message);
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        userList.remove(session);
+    }
+}
